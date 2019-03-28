01eddd5b28df1579ede7ab9689e53962b53d5c96
Thiago Augusto dos Santos Martins <tasm2@ug3c07.windows.cin.ufpe.br>
2019-03-28 17:01:05 -0300

    primeira parte pronta

---
+++ Client/client.py
@@ -0,0 +1,64 @@
+import socket
+from os import system
+import argparse
+
+serverName = "localhost"
+serverPort = 2080
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--message', '-d', type=str,
+                    help='Message to send')
+args = parser.parse_args()
+
+print (args.message)
+
+try:
+    clientSocket.connect((serverName,serverPort))
+except Exception:
+    pass
+
+try:
+    print("Select Database operation: ")
+    print("1 - Download from Database")
+    print("2 - Upload to Database")
+    cmd = input("Select option: ")
+
+    if cmd == "1":
+        system('clear')
+        fileName = input("Enter required file name: ")
+        saveName = input("Enter downloaded file name: ")
+
+        clientSocket.send(fileName.encode())
+
+        fileSize = int(clientSocket.recv(1024).decode())
+        print("Tamanho recebido = " + str(fileSize))
+
+        with open(saveName, "xb") as sf:
+            data = b''
+            while True:
+                print("Receiving file...")
+                aux = clientSocket.recv(1024)
+                if not aux: break
+                data += aux
+                if len(data) >= fileSize: break
+
+            sf.write(data)
+            print("File received!")
+
+    else:
+        print("2")
+    clientSocket.close()
+
+
+        #CMD = 1 == END
+
+except KeyboardInterrupt:
+    escape = True
+except Exception:
+    clientSocket.close()
+
+
+clientSocket.close()
+print("\nBye bye :)")
---
+++ Server/server.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import sys
+import itertools
+import socket
+from socket import socket as Socket
+
+
+def main():
+
+    # Create the server socket (to handle tcp requests using ipv4), make sure
+    # it is always closed by using with statement.
+    with Socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+
+        # The socket stays connected even after this script ends. So in order
+        # to allow the immediate reuse of the socket (so that we can kill and
+        # re-run the server while debugging) we set the following option. This
+        # is potentially dangerous in real code: in rare cases you may get junk
+        # data arriving at the socket.
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        server_socket.bind(('', 2080))
+        server_socket.listen(1)
+
+        print("========================================================")
+        print("==================== Zebra Database ====================")
+        print("========================================================")
+        print("\n\n")
+        print("Up and running!")
+        print("\n\n")
+        contador = 0
+
+        while True:
+            contador += 1
+            with server_socket.accept()[0] as connection_socket:
+                request = connection_socket.recv(1024).decode('ascii')
+
+                size = send_size(request)
+                connection_socket.send(size.encode())
+
+                arq = send_file(request)
+                print("Sending file...")
+                connection_socket.send(arq.encode())
+                print("File sent!")
+            print("\n\nNumber of sent files: " + str(contador) + "\n\n")
+
+    return 0
+
+
+def send_size(request_string):
+    print('Requested file: ' + request_string)
+
+    assert not isinstance(request_string, bytes)
+
+    with open('Database/' + request_string, 'rb') as file:
+        bytesize = len(file.read())*8
+
+    print('Required file size: ' + str(bytesize))
+
+    return str(bytesize)
+
+
+
+def send_file(request_string):
+
+    with open('Database/' + request_string, 'r') as file:
+        binData = file.read()
+
+    return binData
+
+
+
+def http_handle(request_string):
+
+    print('Requested file: ' + request_string)
+
+    assert not isinstance(request_string, bytes)
+
+    contents = ''
+    with open('Database/' + request_string, 'rb') as file:
+        bytesize = len(file.read())*8
+        #contents = file.read()
+    print('contents: ' + contents)
+
+    return contents
+
+if __name__ == "__main__":
+    sys.exit(main())
